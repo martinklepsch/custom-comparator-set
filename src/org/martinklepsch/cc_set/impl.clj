23d779c4208e2768fa465df7832a605ca645a572
Martin Klepsch <martinklepsch@googlemail.com>
2015-11-10 17:10:40 +0100

    add a bunch of commented forms for testing

--- src/org/martinklepsch/cc_set/impl.clj
+++ src/org/martinklepsch/cc_set/impl.clj
@@ -53,3 +53,32 @@
       (print-method x w)
       (when (seq xs) (.write w " ") (recur xs))))
   (.write w "}"))
+
+(comment
+  (.equals (CustomComparatorSet. {"a" {:id "a"}} :id) #{})
+  (.hashCode (CustomComparatorSet. {} :id))
+  (.hashCode #{})
+  (hash #{})
+  (hash (CustomComparatorSet. {} :id))
+  (hash {})
+
+  (def x (CustomComparatorSet. {} :id))
+
+  (conj x {:id 1} {:id 2})
+
+  (set (seq x))
+
+  (contains? (CustomComparatorSet. {"a" {:thing "a"}} :thing) {:thing "a"})
+  (disj (CustomComparatorSet. {"a" {:thing "a"}} :thing) {:thing "a"})
+
+  (set-by :thing {:thing "a"} {:thing "x"} {:thing "b"})
+
+  (require 'collection-check)
+
+  (collection-check/assert-set-like (set-by :x) clojure.test.check.generators/int)
+
+  (.equals #{} (set-by :x))
+
+  (set-by :x)
+
+  )
